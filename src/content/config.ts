ef0b81d38f9731563b15ae34f94b0ba7c5808bdc
Nacho Geoghegan <ignaciog00@gmail.com>
2023-09-25 15:31:45 -0300

    added basic viewtransition animation, so far, it only mathces the blog post images

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,6 +7,7 @@ const blogSchema = z.object({
     updatedDate: z.string().optional(),
     heroImage: z.string().optional(),
     badge: z.string().optional(),
+    slug: z.string().optional(),
 });
 
 const storeSchema = z.object({
