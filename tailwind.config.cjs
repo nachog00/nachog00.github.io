8b17cba64315e774d9e649c80f1e80ebbc4295a0
Nacho Geoghegan <ignaciog00@gmail.com>
2023-09-26 00:28:18 -0300

    added initial support for theme picking, but its not persisting after the transitions.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,8 @@ module.exports = {
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 	daisyui: {
 		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-		darkTheme: "dark", // name of one of the included themes for dark mode
+		darkTheme: "business", // name of one of the included themes for dark mode
+		lightTheme: "corporate", // name of one of the included themes for light mode
 		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
 	  }
 }
